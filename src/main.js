5c59a87588c5ba280a66ae9d0f79383b3ff59063
natsume0718 <moto718program@gmail.com>
2019-11-24 16:34:06 +0900

    firebase導入

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase';
 
 Vue.config.productionTip = false
+
+// firebase 設定
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDh-1t3y_TgvEJYDYgobb6azarvuRKxoOg",
+  authDomain: "vueaddress-bb92a.firebaseapp.com",
+  databaseURL: "https://vueaddress-bb92a.firebaseio.com",
+  projectId: "vueaddress-bb92a",
+  storageBucket: "vueaddress-bb92a.appspot.com",
+  messagingSenderId: "683195636009",
+  appId: "1:683195636009:web:04456c250a434d1a61bf38",
+  measurementId: "G-X4MGW8ST6Q"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
+
 
 new Vue({
   router,
